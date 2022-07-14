e774c6e67c662eea78ec23e7b1f6ccc1b4789710
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2022-07-14 16:24:38 +0900

    レスポンスを rspec で取りやすいように改修

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -2,35 +2,39 @@ class Api::V1::TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :set_todo, only: [:show, :update, :destroy]
 
+   # GET /api/v1/todos
   def index
-    todos = Todo.order(created_at: :desc)
-    render json: { status: 'SUCCESS', message: 'Loaded todos', data: todos }
+    @todos = Todo.all
+    render json: @todos
   end
 
+  # GET /api/v1/todos/1
   def show
-    render json: { status: 'SUCCESS', message: 'Loaded the todo', data: @todo }
+    render @todos
   end
 
+  # POST /api/v1/todos
   def create
-    todo = Todo.new(todo_params)
-    if todo.save
-      render json: { status: 'SUCCESS', data: todo }
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo, status: :created, location: @todo
     else
-      render json: { status: 'ERROR', data: todo.errors }
+      render json: @todo.errors, status: :unprocessable_entity
     end
   end
 
-  def destroy
-    @todo.destroy
-    render json: { status: 'SUCCESS', message: 'Deleted the todo', data: @todo }
-  end
-
+  # PATCH/PUT /todos/1
   def update
     if @todo.update(todo_params)
-      render json: { status: 'SUCCESS', message: 'Updated the todo', data: @todo }
+      render json: @todo
     else
-      render json: { status: 'SUCCESS', message: 'Not updated', data: @todo.errors }
+      render json: @todo.errors, status: :unprocessable_entity
     end
+  end
+
+  # DELETE /todos/1
+  def destroy
+    @todo.destroy
   end
 
   private
